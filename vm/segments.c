09dc93d1aafdef37d544aa9baabc2db5f619af4d
Aurelio Cirella <cirella_aurelio@yahoo.com>
2020-11-26 17:23:58 +0100

    code and text segment management in ELF file added
    Currently not working, to be reviewed.

--- vm/segments.c
+++ vm/segments.c
@@ -20,11 +20,24 @@ int load_page_from_elf(vaddr_t vaddr, int is_code){
     struct addrspace *as = proc_getas();
     struct vnode *v = as->v;
     int result;
-    off_t base = is_code ? as->offset_text_elf : as->offset_data_elf;
-    off_t offset = is_code ? (vaddr - as->as_vbase1) : (vaddr - as->as_vbase2);
-    base &= PAGE_FRAME;
+    off_t segm_base = is_code ? as->offset_text_elf : as->offset_data_elf;
+    off_t segm_offset = is_code ? (vaddr - as->as_vbase1) : (vaddr - as->as_vbase2);
+    off_t elf_offset= segm_base + segm_offset; 
+    
+    elf_offset &= PAGE_FRAME;
+    size_t size, temp; 
+    temp = elf_offset+PAGE_SIZE;
 
-    uio_init(&iov, &u, (void *)vaddr, PAGE_SIZE, base + offset, UIO_READ, is_code);
+    if(is_code && (temp>=as->offset_data_elf)) 
+        temp = as->offset_data_elf-1;    
+    if(!is_code && (elf_offset<as->offset_data_elf))
+        elf_offset = as->offset_data_elf;
+
+    size = temp-elf_offset;
+
+    uio_init(&iov, &u, (void *)vaddr, size, elf_offset, UIO_READ, is_code);
+    // uio_init(&iov, &u, (void *)vaddr, PAGE_SIZE, elf_offset, UIO_READ, is_code);
+
     result = VOP_READ(v, &u);
     if(result)
         return result;
